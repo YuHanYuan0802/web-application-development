47d4c27e161a358e0beb3470b6c6eaa7d891eeb1
Yu <yhanyuan2@gmail.com>
2023-05-22 16:01:06 +0800

    first php script

---
+++ week2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>week2php</title>
+</head>
+
+<body>
+    <?php
+    echo "My first PHP script!" . "<br>";
+    echo date("l") . "<br>";
+    $color = "red";
+    echo "My car is " . $color . "<br>";
+    ?>
+</body>
+
+</html>
