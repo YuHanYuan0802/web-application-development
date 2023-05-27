ea1546bf2effea7c63b68574b1560282c13b1226
KelvinYu223 <yhanyuan@gmail.com>
2023-05-27 13:59:26 +0800

    using multiple for loop to print star

--- week2/star.php
+++ week2/star.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Star</title>
 </head>
-<body>
 
+<body>
+    <?php
+    for ($i = 10; $i > 0; $i--) {
+        for ($j = 0; $j < $i; $j++) {
+            echo "*";
+        }
+        echo "\n <br>";
+    }
+    ?>
 </body>
+
 </html>
