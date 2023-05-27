3454f887450c914b9195781417f8e82920432997
KelvinYu223 <yhanyuan@gmail.com>
2023-05-27 12:49:36 +0800

    set timezone to Singapore

--- week2/date_today.php
+++ week2/date_today.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <title>Today's Date</title>
 </head>
 <body>
@@ -14,6 +16,7 @@
                 <button class="btn btn-lg btn-info" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <select name="Date" id="Date" class="bg-info border border-0">
                         <?php
+                        date_default_timezone_set("Asia/Singapore");
                         for ($i = 1; $i <= 31; $i++) {
                             echo "<option value='Dates'>Date $i</option>";
                         }
