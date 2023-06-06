bc448d39baadeee62de6eba3fb967e10960d730c
KelvinYu223 <yhanyuan@gmail.com>
2023-06-06 16:07:33 +0800

    created sumtwonumber folder

---
+++ week4/sumtwonumber/sumtwonumber.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <title>SumTwoNumber</title>
+</head>
+
+<body>
+    <form action="sumresult.php" class="fs-3" method="post">
+        Number 1: <input type="text" name="number1" style="font-size: large;"><br>
+        Number 2: <input type="text" name="number2" style="font-size: large;"><br>
+        <input type="submit" class="fs-3"><br>
+    </form>
+</body>
+
+</html>
