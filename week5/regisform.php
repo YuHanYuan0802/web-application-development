5f49568d6fcd33eed8378c17673bea5fbc6618f4
KelvinYu223 <yhanyuan@gmail.com>
2023-06-17 02:32:00 +0800

    commit suixide

--- week5/regisform.php
+++ week5/regisform.php
@@ -11,28 +11,16 @@
 
 <body>
     <div class="container">
-        <form action="regissuccess.php" method="post" class="needs-validation was-validated">
+        <form action="" method="post">
             <div class="mb-3">
                 <label for="firstname" class="form-label">First Name</label>
-                <input type="text" class="form-control" id="firstname" name="firstname" required>
+                <input type="text" class="form-control" id="firstname" name="firstname" value="<?php echo isset($_POST["firstname"]) ? $_POST["firstname"] : ''; ?>" required>
                 <div id="emailHelp" class="form-text">Your first name.</div>
-                <div class="valid-feedback">
-                    Looks good!
-                </div>
-                <div class="invalid-feedback">
-                    Please enter your first name.
-                </div>
             </div>
             <div class="mb-3">
                 <label for="lastname" class="form-label">Last Name</label>
-                <input type="text" class="form-control" id="lastname" name="lastname" required>
+                <input type="text" class="form-control" id="lastname" name="lastname" value="<?php echo isset($_POST["lastname"]) ? $_POST["lastname"] : ''; ?>" required>
                 <div id="emailHelp" class="form-text">Your last name.</div>
-                <div class="valid-feedback">
-                    Looks good!
-                </div>
-                <div class="invalid-feedback">
-                    Please enter your last name.
-                </div>
             </div>
             <div class="mb-3">
                 <label for="birthday" class="form-label">Choose your birthday</label><br>
@@ -73,19 +61,28 @@
             <div class="mb-3 row">
                 <label for="gender" class="form-label">Gender</label><br>
                 <div class="form-check col ms-3">
-                    <input class="form-check-input" type="radio" name="gender" id="male" value="Male" checked>
+                    <input class="form-check-input" type="radio" name="gender" id="male" value="male" checked required>
                     <label class="form-check-label" for="male">
                         Male
                     </label>
                 </div>
                 <div class="form-check col">
-                    <input class="form-check-input" type="radio" name="gender" id="female" value="Female">
+                    <!-- check name gender is null or not and if the gender equal to value male, then print checked as the attribute of the radio button -->
+                    <input class="form-check-input" type="radio" name="gender" id="female" value="female" <?php
+                                                                                                            if (isset($_POST["gender"]) && $_POST["gender"] == "female") {
+                                                                                                                echo "checked";
+                                                                                                            }
+                                                                                                            ?> required>
                     <label class="form-check-label" for="female">
                         Female
                     </label>
                 </div>
                 <div class="form-check col">
-                    <input class="form-check-input" type="radio" name="gender" id="other" value="Other">
+                    <input class="form-check-input" type="radio" name="gender" id="other" value="other" <?php
+                                                                                                        if (isset($_POST["gender"]) && $_POST["gender"] == "other") {
+                                                                                                            echo "checked";
+                                                                                                        }
+                                                                                                        ?> required>
                     <label class="form-check-label" for="other">
                         Other
                     </label>
@@ -93,41 +90,89 @@
             </div>
             <div class="mb-3">
                 <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="username" aria-describedby="emailHelp" minlength="6" name="username" required>
+                <input type="text" class="form-control" id="username" aria-describedby="emailHelp" minlength="6" name="username" value="<?php echo isset($_POST["username"]) ? $_POST["username"] : ''; ?>" required>
                 <div id="emailHelp" class="form-text">Your username.</div>
-                <div class="valid-feedback">
-                    Looks good!
-                </div>
-                <div class="invalid-feedback">
-                    Please enter your username.
-                </div>
             </div>
             <div class="mb-3">
                 <label for="InputPassword" class="form-label">Password</label>
-                <input type="password" class="form-control" id="InputPassword" minlength="6" name="password" required>
+                <input type="password" class="form-control" id="InputPassword" minlength="6" name="password" value="<?php echo isset($_POST["password"]) ? $_POST["password"] : ''; ?>" required>
                 <div id="emailHelp" class="form-text">Your password.</div>
             </div>
             <div class="mb-3">
                 <label for="ConfirmPassword" class="form-label">Confirm Password</label>
-                <input type="password" class="form-control" id="ConfirmPassword" name="confirmpassword" required>
+                <input type="password" class="form-control" id="ConfirmPassword" name="confirmpassword" value="<?php echo isset($_POST["confirmpassword"]) ? $_POST["confirmpassword"] : ''; ?>" required>
                 <div id="emailHelp" class="form-text">Confirm your password.</div>
             </div>
             <div class="mb-3">
                 <label for="InputEmail" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" name="email" required>
+                <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" name="email" value="<?php echo isset($_POST["email"]) ? $_POST["email"] : ''; ?>" required>
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                <div class="valid-feedback">
-                    Looks good!
-                </div>
-                <div class="invalid-feedback">
-                    Please enter your email.
-                </div>
             </div>
             <div class="mb-2">
-                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                <button id="submit" type="submit" class="btn btn-primary" name="submit">Submit</button>
             </div>
         </form>
     </div>
+    <?php
+    if (isset($_POST["submit"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $confirmpassword = $_POST["confirmpassword"];
+        $symbol = preg_match('/[+$()%@#]/', $password); //check the string have included those invalid symbols.
+        $uppercase = preg_match('/[A-Z]{6,}/', $password); //check all uppercase without lowercase and number.
+        $lowercase = preg_match('/[a-z]{6,}/', $password); //check all lowercase without uppercase and number.
+        $number =  preg_match('/[0-9]{6,}/', $password); //check all number without uppercase and lowercase.
+        $userpattern = '/^[a-zA-Z][a-zA-Z0-9_-]{5,}$/';
+        $namecase = 0;
+        $passcase = 0;
+        $cfmcase = 0;
+        if (!preg_match($userpattern, $username)) {
+            $namecase = 1;
+        } 
+
+        if ($symbol || $uppercase || $lowercase || $number) { //if matches any word, symbol or number case return true.
+            $passcase = 1;
+        } 
+
+        if ($password != $confirmpassword) { //if not matches with previous password return true.
+            $cfmcase = 1;
+        }
+
+        if ($namecase != 1 && $passcase != 1 && $cfmcase != 1) {
+            $namecase = 2;
+        }
+
+        switch ($namecase) {
+            case '1':
+                echo "<div class='alert alert-danger container' role='alert'>";
+                echo "Please enter minimum 6 characters and first character cannot be number, only _ or - is allowed.";
+                echo "</div>";
+                break;
+
+            case '2':
+                echo "<div class='alert alert-success container' role='alert'>";
+                echo "Register successful" . "<br>" . "Welcome" . " " . $username;
+                echo "</div>";
+
+        }
+
+        switch ($passcase) {
+            case '1':
+                echo "<div class='alert alert-danger container' role='alert'>";
+                echo "Please enter minimum 6 characters, at least 1 capital letter, 1 small letter, 1 number, and NO symbols like +$()% (@#) allowed.";
+                echo "</div>";
+                break;
+        }
+
+        switch ($cfmcase) {
+            case '1':
+                echo "<div class='alert alert-danger container' role='alert'>";
+                echo "Please enter the same password.";
+                echo "</div>";
+                break;
+        }
+    }
+    ?>
 </body>
 
 </html>
