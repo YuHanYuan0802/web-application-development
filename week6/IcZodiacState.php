cc63517577d08b5ebd1a5a82db3a4b3f361c6452
KelvinYu223 <yhanyuan@gmail.com>
2023-06-24 03:26:47 +0800

    added country images and if else statement to display those image

--- week6/IcZodiacState.php
+++ week6/IcZodiacState.php
@@ -43,7 +43,7 @@
             </select>
             <br>
             <br>
-            <input type="submit" name="submit" value="Verify and Submit"><br>
+            <input type="submit" name="submit" value="Verify and Submit">
         </form>
     </div>
 
@@ -60,10 +60,98 @@
             $cnzodiac = array("Mouse", "Cow", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Chicken", "Dog", "Pig");
             $numzodiac = $year + 9;
             $arrayzodiacnum = $numzodiac % 12;
+            $countrycode = substr($ic, 5, -5); //substr only return specific part of string with string starting point and length, positive number returned with start of the string and the negative number return from end of the string
             echo "<br><div class='fs-3 text-center'>";
-            echo $ic;
+            echo "$ic";
+            echo "<br>";
+            if ($countrycode === "01" || $countrycode === "21"  || $countrycode === "22"  || $countrycode === "23" || $countrycode === "24") {
+                echo "Your are from Johor";
+                echo "<br>";
+                echo "<img src = 'img/johor.jpg' alt = 'Johor'>";
+                echo "<br>";
+            } else if ($countrycode === "02" || $countrycode === "25" || $countrycode === "26" || $countrycode === "27") {
+                echo "Your are from Kedah";
+                echo "<br>";
+                echo "<img src = 'img/kedah.jpg' alt = 'Kedah'>";
+                echo "<br>";
+            } else if ($countrycode === "03" || $countrycode === "28" || $countrycode === "29") {
+                echo "Your are from Kelantan";
+                echo "<br>";
+                echo "<img src = 'img/kelantan.jpg' alt = 'Kelatan'>";
+                echo "<br>";
+            } else if ($countrycode === "04" || $countrycode === "30") {
+                echo "Your are from Malacca";
+                echo "<br>";
+                echo "<img src = 'img/malacca.jpg' alt = 'Malacca'>";
+                echo "<br>";
+            } else if ($countrycode === "05" || $countrycode === "31" || $countrycode === "59") {
+                echo "Your are from Negeri Sembilan";
+                echo "<br>";
+                echo "<img src = 'img/sembilan.jpg' alt = 'Sembilan'>";
+                echo "<br>";
+            } else if ($countrycode === "06" || $countrycode === "32" || $countrycode === "33") {
+                echo "Your are from Pahang";
+                echo "<br>";
+                echo "<img src = 'img/pahang.jpg' alt = 'Pahang'>";
+                echo "<br>";
+            } else if ($countrycode === "07" || $countrycode === "34" || $countrycode === "35") {
+                echo "Your are from Penang";
+                echo "<br>";
+                echo "<img src = 'img/penang.jpg' alt = 'Penang'>";
+                echo "<br>";
+            } else if ($countrycode === "08" || $countrycode === "36" || $countrycode === "37" || $countrycode === "38" || $countrycode === "39") {
+                echo "Your are from Perak";
+                echo "<br>";
+                echo "<img src = 'img/perak.jpg' alt = 'Perak'>";
+                echo "<br>";
+            } else if ($countrycode === "09" || $countrycode === "40") {
+                echo "Your are from Perlis";
+                echo "<br>";
+                echo "<img src = 'img/perlis.jpg' alt = 'Perlis'>";
+                echo "<br>";
+            } else if ($countrycode === "10" || $countrycode === "41" || $countrycode === "42" || $countrycode === "43" || $countrycode === "44") {
+                echo "Your are from Selangor";
+                echo "<br>";
+                echo "<img src = 'img/selangor.jpg' alt = 'Selangor'>";
+                echo "<br>";
+            } else if ($countrycode === "11" || $countrycode === "45" || $countrycode === "46") {
+                echo "Your are from Terengganu";
+                echo "<br>";
+                echo "<img src = 'img/terengganu.jpg' alt = 'Terengganu'>";
+                echo "<br>";
+            } else if ($countrycode === "12" || $countrycode === "47" || $countrycode === "48" || $countrycode === "49") {
+                echo "Your are from Sabah";
+                echo "<br>";
+                echo "<img src = 'img/sabah.jpg' alt = 'Sabah'>";
+                echo "<br>";
+            } else if ($countrycode === "13" || $countrycode === "50" || $countrycode === "51" || $countrycode === "52" || $countrycode === "53") {
+                echo "Your are from Sarawak";
+                echo "<br>";
+                echo "<img src = 'img/sarawak.jpg' alt = 'Sarawak'>";
+                echo "<br>";
+            } else if ($countrycode === "14" || $countrycode === "54" || $countrycode === "55" || $countrycode === "56" || $countrycode === "57") {
+                echo "Your are from Federal Territory of Kuala Lumpur";
+                echo "<br>";
+                echo "<img src = 'img/kl.jpg' alt = 'KL'>";
+                echo "<br>";
+            } else if ($countrycode === "15" || $countrycode === "58") {
+                echo "Your are from Federal Territory of Labuan";
+                echo "<br>";
+                echo "<img src = 'img/labuan.jpg' alt = 'Labuan'>";
+                echo "<br>";
+            } else if ($countrycode === "16") {
+                echo "Your are from Federal Territory of Putrajaya";
+                echo "<br>";
+                echo "<img src = 'img/putrajaya.jpg' alt = 'Putrajaya'>";
+                echo "<br>";
+            } else {
+                echo "<br><div class='alert alert-danger container'>";
+                echo "Not found";
+                echo "</div>";
+            }
             echo "<br>";
             echo $month[$arraymonth - 1] . " " . $date . ", " . $year . " " . "<br>Age: " . $age;
+            echo "<br>";
             echo "<br>";
             echo "Your Chinese Zodiac is: " . $cnzodiac[$arrayzodiacnum - 1];
             echo "<br>";
