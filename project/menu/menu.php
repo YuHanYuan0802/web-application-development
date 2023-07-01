5420be711bee5c0cfcef676960843dc09646c795
KelvinYu223 <yhanyuan@gmail.com>
2023-07-01 16:09:27 +0800

    rename nav to project

--- project/menu/menu.php
+++ project/menu/menu.php
@@ -1,7 +1,7 @@
     <div class="page-header">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid d-flex">
-                <a class="navbar-brand" href="#">Create Product</a>
+                <a class="navbar-brand" href="#">Project</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
