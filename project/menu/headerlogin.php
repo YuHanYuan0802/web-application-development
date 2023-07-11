2540c4d27da2353b2e9c21b91b39a9966146f266
KelvinYu223 <yhanyuan@gmail.com>
2023-07-11 19:11:21 +0800

    create login header

---
+++ project/menu/headerlogin.php
@@ -0,0 +1,16 @@
+<div class="page-header">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid d-flex">
+                <a class="navbar-brand" href="#">Login Form</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link" href="login.php">Login</a>
+                        <a class="nav-link" href="#">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
