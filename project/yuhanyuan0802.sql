58d55451a0fd9a162a63dea61c5c2ccbf79f6a96
KelvinYu223 <yhanyuan@gmail.com>
2023-07-12 12:43:55 +0800

    update database file

--- project/yuhanyuan0802.sql
+++ project/yuhanyuan0802.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jul 10, 2023 at 09:42 AM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2023 at 06:43 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,17 +27,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `contact`
 --
 
-DROP TABLE IF EXISTS `contact`;
-CREATE TABLE IF NOT EXISTS `contact` (
-  `contact_id` int NOT NULL,
-  `first_name` varchar(20) CHARACTER SET utf8mb4  NOT NULL,
-  `last_name` varchar(20) CHARACTER SET utf8mb4 NOT NULL,
+CREATE TABLE `contact` (
+  `contact_id` int(11) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
   `email` text NOT NULL,
-  `phone` int NOT NULL,
+  `phone` int(11) NOT NULL,
   `address` text NOT NULL,
-  `message` text NOT NULL,
-  PRIMARY KEY (`contact_id`),
-  UNIQUE KEY `first_name` (`first_name`,`last_name`)
+  `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -46,29 +43,26 @@ CREATE TABLE IF NOT EXISTS `contact` (
 -- Table structure for table `customers`
 --
 
-DROP TABLE IF EXISTS `customers`;
-CREATE TABLE IF NOT EXISTS `customers` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) CHARACTER SET utf8mb4 NOT NULL,
+CREATE TABLE `customers` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `first_name` varchar(20) NOT NULL,
   `last_name` varchar(20) NOT NULL,
-  `password` text NOT NULL,
+  `password` varchar(255) NOT NULL,
   `email` varchar(30) NOT NULL,
   `date_of_birth` date NOT NULL,
-  `registration_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `gender` enum('male','female') CHARACTER SET utf8mb4 NOT NULL,
-  `status` enum('active','Inactive') NOT NULL DEFAULT 'active',
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+  `registration_date_time` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `gender` enum('male','female') NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `customers`
 --
 
 INSERT INTO `customers` (`user_id`, `username`, `first_name`, `last_name`, `password`, `email`, `date_of_birth`, `registration_date_time`, `gender`, `status`) VALUES
-(1, 'yuhanyuan0802', 'yu', 'hanyuan', '123456', '1@gmail.com', '2023-07-05', '2023-07-10 16:19:00', 'male', 'active'),
-(2, 'sinkuan123', 'lim', 'sinkuan', '123456', 'sinkuan@gmail.com', '2023-07-04', '2023-07-10 16:25:00', 'male', 'active');
+(1, 'yuhanyuan0802', 'yu', 'hanyuan', '$2y$10$h1Qz2G2mNwn6QYMQvCDxE.4KF8/ygQinhkxyWrbxD4jNRMNRk7/ay', 'hanyuan@gmail.com', '2002-01-08', '2023-07-11 21:54:00', 'male', 'active'),
+(2, 'skychong', 'sky', 'chong', '$2y$10$zG8cPs1zA6e3qyt6rayOTeoaLNUrJdpEGbNVBr1hrIPX.TqQLx0kq', 'chong@gmail.com', '2002-06-01', '2023-07-11 23:00:00', 'male', 'inactive');
 
 -- --------------------------------------------------------
 
@@ -76,19 +70,17 @@ INSERT INTO `customers` (`user_id`, `username`, `first_name`, `last_name`, `pass
 -- Table structure for table `products`
 --
 
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `description` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `description` text NOT NULL,
   `price` double NOT NULL,
   `promote_price` float NOT NULL,
   `manufacture_date` datetime NOT NULL,
   `expired_date` datetime NOT NULL,
   `created` datetime NOT NULL,
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+  `modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `products`
@@ -102,6 +94,46 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `promote_price`, `
 (5, 'Mouse', 'Very useful if you love your computer.', 11.35, 10, '2022-07-05 13:36:09', '2023-07-09 13:36:09', '2015-08-02 12:17:58', '2023-07-10 05:37:40'),
 (6, 'Earphone', 'You need this one if you love music.', 7, 6, '2022-07-03 13:36:09', '2023-07-09 13:36:09', '2015-08-02 12:18:21', '2023-07-10 05:37:40'),
 (7, 'Pillow', 'Sleeping well is important.', 8.99, 7, '2022-07-03 13:36:09', '2023-07-09 13:36:09', '2015-08-02 12:18:56', '2023-07-10 05:37:40');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`contact_id`),
+  ADD UNIQUE KEY `first_name` (`first_name`,`last_name`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
