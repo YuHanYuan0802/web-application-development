fc90fd1134a263455e6c57f6b9364ec23607545c
KelvinYu223 <yhanyuan@gmail.com>
2023-07-12 20:19:42 +0800

    using if else to verify the session

--- project/customer_read_one.php
+++ project/customer_read_one.php
@@ -1,5 +1,9 @@
 <?php
-session_start();
+if (!empty($_SESSION['login'])) {
+    session_start();
+} else {
+    header('location:login.php');
+}
 ?>
 <!DOCTYPE HTML>
 <html>
--- project/index.php
+++ project/index.php
@@ -1,5 +1,9 @@
 <?php
-session_start();
+if (!empty($_SESSION['login'])) {
+    session_start();
+} else {
+    header('location:login.php');
+}
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -13,14 +17,7 @@ session_start();
 <body>
     <!-- container -->
     <div class="container">
-        <?php
-        include 'menu/menu.php';
-        if (! empty($_SESSION['login'])){
-            echo "Session start. This is home landing page";
-        }else {
-            header('location:login.php');
-        }
-        ?>
+        This is home landing page
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
