c0197864f42c050134c56093901e77e4e6db4688
Yu <yhanyuan2@gmail.com>
2023-07-17 16:29:22 +0800

    update databse sql file with category table

--- project/yuhanyuan0802.sql
+++ project/yuhanyuan0802.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 16, 2023 at 08:09 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 17, 2023 at 08:27 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,18 +24,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(20) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `category_name` (`category_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`, `description`) VALUES
+(1, 'Sport equipment', 'Balls, racket, net, etc'),
+(2, 'Food', 'Main dish or side dish'),
+(3, 'Beverage', 'Soft drink, juices, wine, etc');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `contact`
 --
 
-CREATE TABLE `contact` (
-  `contact_id` int(11) NOT NULL,
-  `first_name` varchar(20) NOT NULL,
-  `last_name` varchar(20) NOT NULL,
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `contact_id` int NOT NULL,
+  `first_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `last_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` text NOT NULL,
-  `phone` int(11) NOT NULL,
+  `phone` int NOT NULL,
   `address` text NOT NULL,
-  `message` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `message` text NOT NULL,
+  PRIMARY KEY (`contact_id`),
+  UNIQUE KEY `first_name` (`first_name`,`last_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -43,26 +70,30 @@ CREATE TABLE `contact` (
 -- Table structure for table `customers`
 --
 
-CREATE TABLE `customers` (
-  `user_id` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `first_name` varchar(20) NOT NULL,
   `last_name` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `password` text NOT NULL,
   `email` varchar(30) NOT NULL,
   `date_of_birth` date NOT NULL,
-  `registration_date_time` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `gender` enum('male','female') NOT NULL,
-  `status` enum('active','inactive') NOT NULL DEFAULT 'active'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `registration_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `gender` enum('male','female') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `status` enum('active','Inactive') NOT NULL DEFAULT 'active',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `customers`
 --
 
 INSERT INTO `customers` (`user_id`, `username`, `first_name`, `last_name`, `password`, `email`, `date_of_birth`, `registration_date_time`, `gender`, `status`) VALUES
-(1, 'yuhanyuan0802', 'yu', 'hanyuan', '$2y$10$h1Qz2G2mNwn6QYMQvCDxE.4KF8/ygQinhkxyWrbxD4jNRMNRk7/ay', 'hanyuan@gmail.com', '2002-01-08', '2023-07-11 21:54:00', 'male', 'active'),
-(2, 'skychong', 'sky', 'chong', '$2y$10$zG8cPs1zA6e3qyt6rayOTeoaLNUrJdpEGbNVBr1hrIPX.TqQLx0kq', 'chong@gmail.com', '2002-06-01', '2023-07-11 23:00:00', 'male', 'inactive');
+(1, 'yuhanyuan0802', 'yu', 'hanyuan', '123456', '1@gmail.com', '2023-07-05', '2023-07-10 16:19:00', 'male', 'active'),
+(2, 'sinkuan123', 'lim', 'sinkuan', '123456', 'sinkuan@gmail.com', '2023-07-04', '2023-07-10 16:25:00', 'male', 'active'),
+(3, 'yuhanyuan', 'yu', 'hanyuan', '$2y$10$iE51c.gSVoaVu0u.2KtebOOGhs4q2RM50oZWeJoKw2oSaV1yD3xBK', 'yu@gmail.com', '2023-07-01', '2023-07-17 14:21:00', 'male', 'active');
 
 -- --------------------------------------------------------
 
@@ -70,70 +101,47 @@ INSERT INTO `customers` (`user_id`, `username`, `first_name`, `last_name`, `pass
 -- Table structure for table `products`
 --
 
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `description` text NOT NULL,
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `category_id` int NOT NULL,
+  `description` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `price` double NOT NULL,
   `promote_price` float NOT NULL,
   `manufacture_date` datetime NOT NULL,
   `expired_date` datetime NOT NULL,
   `created` datetime NOT NULL,
-  `modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `description`, `price`, `promote_price`, `manufacture_date`, `expired_date`, `created`, `modified`) VALUES
-(1, 'Basketball', 'A ball used in the NBA.', 49.99, 0, '2022-07-01 13:36:09', '2023-07-09 13:36:09', '2015-08-02 12:04:03', '2023-07-10 05:56:21'),
-(2, 'Gatorade', 'This is a very good drink for athletes.', 1.99, 1, '2022-07-06 13:36:09', '2023-07-09 13:36:09', '2015-08-02 12:14:29', '2023-07-10 05:37:40'),
-(3, 'Eye Glasses', 'It will make you read better.', 6, 5, '2022-07-05 13:36:09', '2023-07-09 13:36:09', '2015-08-02 12:15:04', '2023-07-10 05:37:40'),
-(4, 'Trash Can', 'It will help you maintain cleanliness.', 3.95, 2, '2022-07-05 13:36:09', '2023-07-06 13:36:09', '2015-08-02 12:16:08', '2023-07-10 05:37:40'),
-(5, 'Mouse', 'Very useful if you love your computer.', 11.35, 10, '2022-07-05 13:36:09', '2023-07-09 13:36:09', '2015-08-02 12:17:58', '2023-07-10 05:37:40'),
-(6, 'Earphone', 'You need this one if you love music.', 7, 6, '2022-07-03 13:36:09', '2023-07-09 13:36:09', '2015-08-02 12:18:21', '2023-07-10 05:37:40'),
-(7, 'Pillow', 'Sleeping well is important.', 8.99, 7, '2022-07-03 13:36:09', '2023-07-09 13:36:09', '2015-08-02 12:18:56', '2023-07-10 05:37:40');
+INSERT INTO `products` (`id`, `name`, `category_id`, `description`, `price`, `promote_price`, `manufacture_date`, `expired_date`, `created`, `modified`) VALUES
+(1, 'Basketball', 1, 'A ball used in the NBA.', 49.99, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2015-08-02 12:04:03', '2015-08-05 22:59:18'),
+(3, 'Gatorade', 3, 'This is a very good drink for athletes.', 1.99, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2015-08-02 12:14:29', '2015-08-05 22:59:18'),
+(4, 'Eye Glasses', 1, 'It will make you read better.', 6, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2015-08-02 12:15:04', '2015-08-05 22:59:18'),
+(5, 'Trash Can', 1, 'It will help you maintain cleanliness.', 3.95, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2015-08-02 12:16:08', '2015-08-05 22:59:18'),
+(6, 'Mouse', 1, 'Very useful if you love your computer.', 11.35, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2015-08-02 12:17:58', '2015-08-05 22:59:18'),
+(7, 'Earphone', 1, 'You need this one if you love music.', 7, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2015-08-02 12:18:21', '2015-08-05 22:59:18'),
+(8, 'Pillow', 1, 'Sleeping well is important.', 8.99, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2015-08-02 12:18:56', '2015-08-05 22:59:18'),
+(9, 'Coca cola', 3, 'Soft drink', 5, 2.5, '2023-07-07 00:00:00', '2023-07-17 00:00:00', '2023-07-17 08:25:43', '2023-07-17 08:25:43'),
+(10, 'Pepsi', 3, 'Soft drink', 4, 2, '2023-07-13 00:00:00', '2023-07-17 00:00:00', '2023-07-17 08:27:24', '2023-07-17 08:27:24');
 
 --
--- Indexes for dumped tables
+-- Constraints for dumped tables
 --
 
 --
--- Indexes for table `contact`
---
-ALTER TABLE `contact`
-  ADD PRIMARY KEY (`contact_id`),
-  ADD UNIQUE KEY `first_name` (`first_name`,`last_name`);
-
---
--- Indexes for table `customers`
---
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- Indexes for table `products`
+-- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `customers`
---
-ALTER TABLE `customers`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
