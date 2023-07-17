2aff1a4c3901d5a8d452826879f8d8e728c50a49
Yu <yhanyuan2@gmail.com>
2023-07-17 17:55:01 +0800

    remove redundant code

--- project/index.php
+++ project/index.php
@@ -18,11 +18,7 @@ session_start();
         ?>
         Welcome
         <?php
-        if (empty($_SESSION)) {
-            header('location:login.php');
-        } else {
             echo " " . $_SESSION['username'];
-        }
         ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
