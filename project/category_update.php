6ec4aa4c3ca2115a1f2f639b60d2ba77c325a4cb
KelvinYu223 <yhanyuan@gmail.com>
2023-07-20 20:50:24 +0800

    created category

---
+++ project/category_update.php
@@ -0,0 +1,123 @@
+<?php
+include 'config/session.php';
+?>
+
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Update product</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <!-- custom css →
+    <style>
+       .m-r-1em{ margin-right:1em; }
+       .m-b-1em{ margin-bottom:1em; }
+       .m-l-1em{ margin-left:1em; }
+       .mt0{ margin-top:0; }
+    </style>-->
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <?php
+            include 'menu/menu.php';
+            ?>
+            <p>Update Category</p>
+        </div>
+        <!-- PHP read record by ID will be here -->
+        <?php
+        // get passed parameter value, in this case, the record ID
+        // isset() is a PHP function used to verify if a value is there or not
+        $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+
+        //include database connection
+        include 'config/database.php';
+
+        // read current record's data
+        try {
+            // prepare select query
+            $query = "SELECT  category_name, description FROM category WHERE category_id = ? LIMIT 0,1";
+            $stmt = $con->prepare($query);
+
+            // this is the first question mark
+            $stmt->bindParam(1, $id);
+
+            // execute our query
+            $stmt->execute();
+
+            // store retrieved row to a variable
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // values to fill up our form
+            $category_name = $row['category_name'];
+            $description = $row['description'];
+        }
+
+        // show error
+        catch (PDOException $exception) {
+            die('ERROR: ' . $exception->getMessage());
+        }
+        ?>
+
+        <!-- HTML form to update record will be here -->
+        <!-- PHP post to update record will be here -->
+        <?php
+        // check if form was submitted
+        if ($_POST) {
+            try {
+                // write update query
+                // in this case, it seemed like we have so many fields to pass and
+                // it is better to label them and not use question marks
+                $query = "UPDATE category SET category_name=:category_name, description=:description WHERE category_id = :id";
+                // prepare query for excecution
+                $stmt = $con->prepare($query);
+                // posted values
+                $category_name = htmlspecialchars(strip_tags($_POST['category_name']));
+                $description = htmlspecialchars(strip_tags($_POST['description']));
+                // bind the parameters
+                $stmt->bindParam(':category_name', $category_name);
+                $stmt->bindParam(':description', $description);
+                $stmt->bindParam(':id', $id);
+                // Execute the query
+                if ($stmt->execute()) {
+                    echo "<div class='alert alert-success'>Record was updated.</div>";
+                } else {
+                    echo "<div class='alert alert-danger'>Unable to update record. Please try again.</div>";
+                }
+            }
+            // show errors
+            catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
+            }
+        } ?>
+
+        <!--we have our html form here where new record information can be updated-->
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Category Name</td>
+                    <td><input type='text' name='category_name' value="<?php echo htmlspecialchars($category_name, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td><textarea name='description' class='form-control'><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></textarea></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save Changes' class='btn btn-primary' />
+                        <a href='category_read.php' class='btn btn-danger'>Back to read category</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+    </div>
+    <!-- end .container -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
