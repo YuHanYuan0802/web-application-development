ddb04a95d20048f7841d3c620e48c8483a0abdc9
KelvinYu223 <yhanyuan@gmail.com>
2023-08-13 16:39:14 +0800

    remove sleep function

--- project/config/validate_login.php
+++ project/config/validate_login.php
@@ -2,7 +2,6 @@
 session_start();
 if (empty($_SESSION['username'])) {
     echo '<script>alert("Please Login to continue!!");';
-    sleep(1);
     echo 'window.location.href="login.php";';
     echo '</script>';
 }
