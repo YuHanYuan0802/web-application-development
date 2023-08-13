2afd360264fe216f94101dec75ad4ca931655399
KelvinYu223 <yhanyuan@gmail.com>
2023-08-13 16:40:08 +0800

    created most buy customers, top and never buy products

--- project/index.php
+++ project/index.php
@@ -15,11 +15,10 @@ include 'config/validate_login.php';
     <div class="container">
         <?php
         include 'menu/menu.php';
-        ?>
-        Welcome
-        <?php
         include 'config/database.php';
-        echo " " . $_SESSION['username'];
+
+        echo "<h5 class = 'text-center'>Welcome Back " . $_SESSION['username'] . "</h5>";
+
         $cusnumquery = "SELECT * FROM customers";
         $cusnumstmt = $con->prepare($cusnumquery);
         $cusnumstmt->execute();
@@ -39,6 +38,19 @@ include 'config/validate_login.php';
         $lateststmt = $con->prepare($latestquery);
         $lateststmt->execute();
 
+        $mostquery = "SELECT customers.username, order_summary.order_date, COUNT(order_id) AS count FROM order_summary INNER JOIN customers ON customers.user_id=order_summary.order_id GROUP BY customer_id ORDER BY COUNT(order_id) DESC LIMIT 1";
+        $moststmt = $con->prepare($mostquery);
+        $moststmt->execute();
+
+        $topquery = "SELECT products.name, products.description, category.category_name FROM order_detail INNER JOIN products ON products.id=order_detail.product_id INNER JOIN category ON category.category_id=products.category_id GROUP BY order_detail.product_id LIMIT 5";
+        $topstmt = $con->prepare($topquery);
+        $topstmt->execute();
+        $counttop = $topstmt->rowCount();
+
+        $nobuyquery = "SELECT products.name, products.description, category.category_name FROM products INNER JOIN category ON category.category_id=products.category_id WHERE NOT EXISTS(SELECT products.id FROM order_detail WHERE order_detail.product_id=products.id) LIMIT 3";
+        $nobuystmt = $con->prepare($nobuyquery);
+        $nobuystmt->execute();
+        $countnobuy = $nobuystmt->rowCount();
         ?>
         <div class="d-flex justify-content-around">
             <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded flex-fill">
@@ -64,13 +76,13 @@ include 'config/validate_login.php';
             </div>
         </div>
         <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded">
-            <?php 
+            <?php
             $total = 0;
             while ($latestrow = $lateststmt->fetch(PDO::FETCH_ASSOC)) {
                 extract($latestrow);
                 echo "<h5>Our Latest Order Make By </h5>";
                 echo "<h5><strong>$username</strong></h5>";
-                echo "<h5>Bought $quantity</h5>";
+                echo "<h5>Bought <strong>$quantity</strong> </h5>";
                 echo "<h5><strong>$name</strong></h5>";
                 echo "<h5>And Order At</h5>";
                 echo "<h5><strong>$order_date</strong></h5>";
@@ -87,39 +99,53 @@ include 'config/validate_login.php';
         </div>
 
         <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded">
-            TODO highest order id and summary
+            <?php
+            $total = 0;
+            while ($mostrow = $moststmt->fetch(PDO::FETCH_ASSOC)) {
+                extract($mostrow);
+                echo "<h5>The Most Order Make By </h5>";
+                echo "<h5><strong>$username</strong></h5>";
+                echo "<h5>With " . "<strong>" . $mostrow['count'] . "</strong>" .  " Order</h5>";
+                echo "<h5>At</h5>";
+                echo "<h5><strong>$order_date</strong></h5>";
+            }
+            ?>
         </div>
 
-        <div class="d-flex justify-content-around">
-            <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded flex-fill">
-                TODO top 5 products
-            </div>
-            <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded flex-fill">
-                TODO top 5 products
-            </div>
-            <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded flex-fill">
-                TODO top 5 products
-            </div>
-        </div>
-
-        <div class="d-flex justify-content-center">
-            <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded">
-                TODO top 5 products
-            </div>
-            <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded">
-                TODO top 5 products
+        <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded">
+            <h5>Top 5 Products Ordered by Our Customers</h5>
+            <div class="d-flex justify-content-around flex-wrap">
+                <?php
+                for ($i = 0; $i < $counttop; $i++) {
+                    echo "<div class='shadow p-5 m-5 bg-body-tertiary rounded text-start' style='width:25rem;'>";
+                    $toprow = $topstmt->fetch(PDO::FETCH_ASSOC);
+                    echo "<h5><strong>" . $toprow['name'] . "</strong></h5>";
+                    echo "<h5>Category</h5>";
+                    echo "<h5><strong>" . $toprow['category_name'] . "</strong></h5>";
+                    echo "<h5>Description</h5>";
+                    echo "<h5><strong>" . $toprow['description'] . "</strong></h5>";
+                    echo "</div>";
+                }
+                ?>
             </div>
         </div>
 
-        <div class="d-flex justify-content-center">
-            <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded flex-fill">
-                TODO products never buy
-            </div>
-            <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded flex-fill">
-                TODO products never buy
-            </div>
-            <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded flex-fill">
-                TODO products never buy
+
+        <div class="text-center shadow p-5 m-5 bg-body-tertiary rounded">
+            <h5>New Products From The Store</h5>
+            <div class="d-flex justify-content-around flex-wrap">
+                <?php
+                for ($i = 0; $i < $countnobuy; $i++) {
+                    echo "<div class='shadow p-5 m-5 bg-body-tertiary rounded text-start' style='width:25rem;'>";
+                    $nobuyrow = $nobuystmt->fetch(PDO::FETCH_ASSOC);
+                    echo "<h5><strong>" . $nobuyrow['name'] . "</strong></h5>";
+                    echo "<h5>Category</h5>";
+                    echo "<h5><strong>" . $nobuyrow['category_name'] . "</strong></h5>";
+                    echo "<h5>Description</h5>";
+                    echo "<h5><strong>" . $nobuyrow['description'] . "</strong></h5>";
+                    echo "</div>";
+                }
+                ?>
             </div>
         </div>
     </div>
