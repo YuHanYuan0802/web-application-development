e876dbecc6fa9e211d1bd569209a99fab585b5b7
Yu <yhanyuan2@gmail.com>
2023-08-21 12:13:44 +0800

    remove comment example

--- project/customer_delete.php
+++ project/customer_delete.php
@@ -14,8 +14,7 @@
     try {
         // get record ID
         // isset() is a PHP function used to verify if a value is there or not
-        //exists example
-        // SELECT name FROM products WHERE EXISTS(SELECT products.id FROM order_detail WHERE order_detail.product_id=products.id)
+
         $id = isset($_GET['id']) ? $_GET['id'] :  die('ERROR: Record ID not found.');
 
         $selectimage = "SELECT image FROM customers WHERE user_id = ?";
