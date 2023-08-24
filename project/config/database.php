51bbfd70eee33ee8f3ebc8bbd32d6f2010cd2b45
KelvinYu223 <yhanyuan@gmail.com>
2023-08-24 17:51:53 +0800

    set default timezone in Asia KL

--- project/config/database.php
+++ project/config/database.php
@@ -4,13 +4,14 @@ $host = "localhost";
 $db_name = "yuhanyuan0802";
 $username = "yuhanyuan0802";
 $password = "3_2q7Ay.ici/8nFT";
-$db = mysqli_connect('localhost','yuhanyuan0802','3_2q7Ay.ici/8nFT','yuhanyuan0802');
+$db = mysqli_connect('localhost', 'yuhanyuan0802', '3_2q7Ay.ici/8nFT', 'yuhanyuan0802');
 try {
+    date_default_timezone_set('Asia/Kuala_Lumpur');
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // show error
     // echo "Connected successfully"; 
-}  
+}
 // show error
-catch(PDOException $exception){
-    echo "Connection error: ".$exception->getMessage();
+catch (PDOException $exception) {
+    echo "Connection error: " . $exception->getMessage();
 }
