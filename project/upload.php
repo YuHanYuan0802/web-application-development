7138b0ed0f6a396b51bd9c300ea9395051324ece
KelvinYu223 <yhanyuan@gmail.com>
2023-08-24 14:33:44 +0800

    update upload image error message structure

--- project/upload.php
+++ project/upload.php
@@ -8,31 +8,31 @@ if (!empty($_FILES["image"]["name"])) {
     $file_type = pathinfo($target_file, PATHINFO_EXTENSION);
 
     // error message is empty
-    $file_upload_error_messages = "";
+    $errormessage = array();
 
     $check = getimagesize($_FILES["image"]["tmp_name"]);
     if ($check !== false) {
         // echo "File is an image";
     } else {
-        $file_upload_error_messages .= "<div>Submitted file is not an image.</div>";
+        $errormessage[] = "Submitted file is not an image.";
     }
 
-    // Using imagesx() imagesy()
-    // if ($check['width']>600 || $check['height']>600) {
-    //     $file_upload_error_messages .= "<div>Submitted file is exceed width 600px and height 600px limit.</div>";
-    // }
+    list($width, $height) = $check;
+    if ($width >= 600 || $height >= 600) {
+        $errormessage[] = "Image should be under 600x600.";
+    }
 
     $allowed_file_types = array("jpg", "jpeg", "png", "gif");
     if (!in_array($file_type, $allowed_file_types)) {
-        $file_upload_error_messages .= "<div>Only JPG, JPEG, PNG, GIF files are allowed.</div>";
+        $errormessage[] = "Only JPG, JPEG, PNG, GIF files are allowed.";
     }
 
     if (file_exists($target_file)) {
-        $file_upload_error_messages .= "<div>Image already exists. Try to change file name.</div>";
+        $errormessage[] = "Image already exists. Try to change file name.";
     }
 
     if ($_FILES["image"]["size"] > (512000)) {
-        $file_upload_error_messages .= "<div>Image must be less than 512 KB in size.</div>";
+        $errormessage[] = "Image must be less than 512 KB in size.";
     }
 
     if (!is_dir($target_directory)) {
@@ -40,7 +40,7 @@ if (!empty($_FILES["image"]["name"])) {
     }
 
     // if $file_upload_error_messages is still empty
-    if (empty($file_upload_error_messages)) {
+    if (empty($errormessage)) {
         // it means there are no errors, so try to upload the file
         if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
             // it means photo was uploaded
@@ -49,14 +49,6 @@ if (!empty($_FILES["image"]["name"])) {
             echo "<div>Unable to upload photo.</div>";
             echo "</div>";
         }
-    }
-
-    // if $file_upload_error_messages is NOT empty
-    else {
-        // it means there are some errors, so show them to user
-        echo "<div class='alert alert-danger'>";
-        echo "<div>{$file_upload_error_messages}</div>";
-        echo "</div>";
     }
 } else if ($_SESSION['image'] == "product") {
     $image = "product_image_coming_soon.jpg";
