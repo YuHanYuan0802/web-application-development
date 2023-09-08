8a09e61c95f1b92ccfacf2bb93ead78dcc17485b
KelvinYu223 <yhanyuan@gmail.com>
2023-09-08 23:35:26 +0800

    added error message and basic input

--- email/email.php
+++ email/email.php
@@ -8,30 +8,93 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);
 
 try {
-	$mail->SMTPDebug = 2;									
-	$mail->isSMTP();										
-	$mail->Host	 = "smtp.gmail.com";				
-	$mail->SMTPAuth = true;							
-	$mail->Username = 'yhanyuan2@gmail.com';				
-	$mail->Password = 'wtftcsrnxiwotzsg';					
-	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;		
-	$mail->Port	 = 465;
+	if (isset($_POST['submit'])) {
+		$sender = $_POST['sender'];
+		$receipt = $_POST['receipt'];
+		$subject = $_POST['subject'];
+		$body = $_POST['body'];
 
-	$mail->setFrom('yhanyuan2@gmail.com');		
-	$mail->addAddress('yuhanyuan0802@e.newera.edu.my');
-	
-	$mail->isHTML(true);								
-	$mail->Subject = 'Subject';
-	$mail->Body = 'HTML message body in <b>bold</b> ';
-	$mail->AltBody = 'Body in plain text for non-HTML mail clients';
-    
-    if ($mail->send()) {
-        echo "Mail has been sent successfully!";
-    }else {
-        echo "Failed to send email.";
-    }
-	
-	
+		$error = array();
+
+		//remove sender and password field if using hardcode.
+
+		if (empty($sender)) {
+			$error[] = "Please enter sender." . "<br>";
+		}
+		if (empty($password)) {
+			$error[] = "Please enter password." . "<br>";
+		}
+		if (empty($receipt)) {
+			$error[] = "Please enter recipient." . "<br>";
+		}
+		if (empty($subject)) {
+			$error[] = "Please enter subject." . "<br>";
+		}
+		if (empty($body)) {
+			$error[] = "Please enter body." . "<br>";
+		}
+		if (!empty($error)) {
+			foreach ($error as $displayerror) {
+				echo $displayerror;
+			}
+			echo "<br>";
+		} else {
+
+			$mail->isSMTP();
+			$mail->Host	 = "smtp.gmail.com";
+			$mail->SMTPAuth = true;
+			$mail->Username = $sender;	//sender and password can be hardcode.
+			$mail->Password = $password; //password from app password of your sender email. https://support.google.com/mail/answer/185833?hl=en
+			$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+			$mail->Port	 = 465;
+
+			$mail->setFrom($sender);
+			$mail->addAddress($receipt);
+
+			$mail->isHTML(true);
+			$mail->Subject = $subject;
+			$mail->Body = $body;
+			$mail->AltBody = $body;
+			if ($mail->send()) {
+				echo "Mail has been sent successfully!";
+			} else {
+				echo "Failed to send email.";
+			}
+		}
+	}
 } catch (Exception $e) {
 	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sent Email</title>
+</head>
+
+<body>
+	<form action="" method="post">
+		<label for="sender">Sender</label><br>
+		<input type="text" name="sender" id="sender"><br>
+
+		<label for="password">Password for sender</label><br>
+		<input type="password" name="password" id="password"><br>
+
+		<label for="receipt">Recipient</label><br>
+		<input type="text" name="receipt" id="receipt"><br>
+
+		<label for="subject">Subject</label><br>
+		<input type="text" name="subject" id="subject"><br>
+
+		<label for="body">Body</label><br>
+		<input type="text" name="body" id="body"><br>
+
+		<input type="submit" name="submit" value="Sent">
+	</form>
+</body>
+
+</html>
